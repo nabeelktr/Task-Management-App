ada9d2fd5507da2b1291ec551c8ab97937547dbf
nabeelktr <nabee446@gmail.com>
2024-07-17 23:48:31 +0530

    Complete database setup

--- apps/task-service/src/task-service.module.ts
+++ apps/task-service/src/task-service.module.ts
@@ -2,13 +2,19 @@ import { Module } from '@nestjs/common';
 import { TaskServiceController } from './task-service.controller';
 import { TaskServiceService } from './task-service.service';
 <<<<<<< HEAD
+<<<<<<< HEAD
 import { ConfigModule } from '@nestjs/config';
 import * as Joi from 'joi';
+=======
+import { ConfigModule } from '@nestjs/config';
+import Joi from 'joi';
+>>>>>>> bf6a401 (Complete database setup)
 import { DatabaseModule } from '@app/common/database/database.module';
 import { TaskRepository } from './task.repository';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Task, TaskSchema } from './schemas/task.schems';
 
+<<<<<<< HEAD
 
 @Module({
   imports: [ConfigModule.forRoot({
@@ -25,11 +31,26 @@ import { Task, TaskSchema } from './schemas/task.schems';
   controllers: [TaskServiceController],
   providers: [TaskServiceService, TaskRepository],
 =======
+=======
+>>>>>>> bf6a401 (Complete database setup)
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({
+    isGlobal: true,
+    validationSchema: Joi.object({
+      MONGODB_URI: Joi.string().required()
+    }),
+    envFilePath: "./apps/task-service/.env"
+  }),
+  DatabaseModule,
+  MongooseModule.forFeature([{name: Task.name, schema: TaskSchema}]),
+],
   controllers: [TaskServiceController],
+<<<<<<< HEAD
   providers: [TaskServiceService],
 >>>>>>> f359e91 (Complete setup of Task Service)
+=======
+  providers: [TaskServiceService, TaskRepository],
+>>>>>>> bf6a401 (Complete database setup)
 })
 export class TaskServiceModule {}
