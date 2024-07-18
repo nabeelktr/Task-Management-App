3ddc0a7c55ad4fc31e4dafe8529f37175d6af560
nabeelktr <nabee446@gmail.com>
2024-07-18 17:35:14 +0530

    bug fix

--- apps/api-gateway/src/main.ts
+++ apps/api-gateway/src/main.ts
@@ -1,8 +1,31 @@
 import { NestFactory } from '@nestjs/core';
 import { ApiGatewayModule } from './api-gateway.module';
+import { createProxyMiddleware } from 'http-proxy-middleware';
 
 async function bootstrap() {
-  const app = await NestFactory.create(ApiGatewayModule);
-  await app.listen(3001);
+  const app = await NestFactory.create(ApiGatewayModule, {bodyParser: false});
+  const globalPrefix = 'api';
+  app.setGlobalPrefix(globalPrefix);
+
+  // app.use(
+  //   `/api/v1/task-service/*`,
+  //   createProxyMiddleware({
+  //     target: 'http://task-service:3002/',
+  //     pathRewrite: {
+  //       '/api/v1/task-service': '/',
+  //     },
+  //     changeOrigin: true,
+  //     secure: false,
+  //     on:{
+  //       proxyReq:(proxyReq, req, res) => {
+  //           console.log('[NestMiddleware]: Proxying', req.method, 'request originally made to');
+  //         },
+  //     }
+  //   }),
+  // );
+
+  await app.listen(3001, () => {
+    console.log("Api gateway server is listening..");
+  });
 }
 bootstrap();
--- apps/task-service/src/task-service.module.ts
+++ apps/task-service/src/task-service.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { TaskServiceController } from './task-service.controller';
 import { TaskServiceService } from './task-service.service';
 import { ConfigModule } from '@nestjs/config';
-import Joi from 'joi';
+import * as Joi from 'joi';
 import { DatabaseModule } from '@app/common/database/database.module';
 import { TaskRepository } from './task.repository';
 import { MongooseModule } from '@nestjs/mongoose';
