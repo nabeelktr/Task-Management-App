086ec42dc542289a606cb937a3051126ee57b63b
nabeelktr <nabee446@gmail.com>
2024-07-18 19:02:37 +0530

    change port for task

--- apps/task-service/src/task-service.module.ts
+++ apps/task-service/src/task-service.module.ts
@@ -1,24 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TaskServiceController } from './task-service.controller';
 import { TaskServiceService } from './task-service.service';
-<<<<<<< HEAD
-<<<<<<< HEAD
 import { ConfigModule } from '@nestjs/config';
 import * as Joi from 'joi';
-=======
-import { ConfigModule } from '@nestjs/config';
-<<<<<<< HEAD
-import Joi from 'joi';
->>>>>>> bf6a401 (Complete database setup)
-=======
-import * as Joi from 'joi';
->>>>>>> 3ddc0a7 (bug fix)
 import { DatabaseModule } from '@app/common/database/database.module';
 import { TaskRepository } from './task.repository';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Task, TaskSchema } from './schemas/task.schems';
 
-<<<<<<< HEAD
 
 @Module({
   imports: [ConfigModule.forRoot({
@@ -34,28 +23,5 @@ import { Task, TaskSchema } from './schemas/task.schems';
 ],
   controllers: [TaskServiceController],
   providers: [TaskServiceService, TaskRepository],
-=======
-=======
->>>>>>> bf6a401 (Complete database setup)
-
-@Module({
-  imports: [ConfigModule.forRoot({
-    isGlobal: true,
-    validationSchema: Joi.object({
-      MONGODB_URI: Joi.string().required(),
-      PORT: Joi.number().required(),
-    }),
-    envFilePath: "./apps/task-service/.env"
-  }),
-  DatabaseModule,
-  MongooseModule.forFeature([{name: Task.name, schema: TaskSchema}]),
-],
-  controllers: [TaskServiceController],
-<<<<<<< HEAD
-  providers: [TaskServiceService],
->>>>>>> f359e91 (Complete setup of Task Service)
-=======
-  providers: [TaskServiceService, TaskRepository],
->>>>>>> bf6a401 (Complete database setup)
 })
 export class TaskServiceModule {}
