d88cd3e0e7c7173c928e31925fc08d284b3f04fc
nabeelktr <nabee446@gmail.com>
2024-07-18 15:35:15 +0530

    initiated jwt with passportjs

--- apps/auth/src/main.ts
+++ apps/auth/src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 <<<<<<< HEAD
+<<<<<<< HEAD
 import { RmqService } from '@app/common';
 import { AuthModule } from './auth.module';
 import { RmqOptions } from '@nestjs/microservices';
@@ -15,11 +16,26 @@ async function bootstrap() {
   await app.startAllMicroservices();
   await app.listen(configService.get('PORT'));
 =======
+=======
+import { RmqService } from '@app/common';
+>>>>>>> 99a4d45 (initiated jwt with passportjs)
 import { AuthModule } from './auth.module';
+import { RmqOptions } from '@nestjs/microservices';
+import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AuthModule);
+<<<<<<< HEAD
   await app.listen(3000);
 >>>>>>> b8f7e5e (rabbitmq setup complete)
+=======
+  const rmqService = app.get<RmqService>(RmqService);
+  app.connectMicroservice<RmqOptions>(rmqService.getOptions('AUTH', true));
+  app.useGlobalPipes(new ValidationPipe());
+  const configService = app.get(ConfigService);
+  await app.startAllMicroservices();
+  await app.listen(configService.get('PORT'));
+>>>>>>> 99a4d45 (initiated jwt with passportjs)
 }
 bootstrap();
